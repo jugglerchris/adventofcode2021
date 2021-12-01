d3c38b0e98a38e874d151d006d8186cb10cead96
Chris Emerson <github@mail.nosreme.org>
2021-12-01 20:27:35 +0000

    Update template.

--- examples/template.rs
+++ examples/template.rs
@@ -1,6 +1,6 @@
 use adventofcode2019::{get_input,parse_lines};
 fn main() -> std::io::Result<()>{
-    let input = get_input(1)?;
+    let input = get_input($N)?;
 
     let increments: Vec<u32> = parse_lines(&input);
 
@@ -10,6 +10,7 @@ fn main() -> std::io::Result<()>{
                              .sum::<u32>());
 
     // Part 2
+    /*
     println!("{}", increments.iter()
                              .map(|mass| {
                                  let mut mass = *mass;
@@ -22,6 +23,7 @@ fn main() -> std::io::Result<()>{
                                  fuel
                               })
                              .sum::<u32>());
+    */
 
     Ok(())
 }
