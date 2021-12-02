8130cc62b59b5daff75f2e52c71f4eec908f592d
Chris Emerson <github@mail.nosreme.org>
2021-12-02 06:55:01 +0000

    Day 2 part 2.

--- examples/day2.rs
+++ examples/day2.rs
@@ -40,20 +40,26 @@ fn main() -> std::io::Result<()>{
     }
 
     // Part 2
-    /*
-    println!("{}", increments.iter()
-                             .map(|mass| {
-                                 let mut mass = *mass;
-                                 let mut fuel = 0;
-                                 while mass > 5 {
-                                     let f = mass/3 - 2;
-                                     fuel += f;
-                                     mass = f;
-                                 }
-                                 fuel
-                              })
-                             .sum::<u32>());
-    */
+    {
+        let mut x = 0;
+        let mut y = 0;
+        let mut aim = 0;
+        for command in &commands {
+            match command {
+                Forward(n) => {
+                    x += n;
+                    y += aim*n;
+                }
+                Up(n) => {
+                    aim -= n;
+                }
+                Down(n) => {
+                    aim += n;
+                }
+            }
+        }
+        println!("{}", x*y);
+    }
 
     Ok(())
 }
