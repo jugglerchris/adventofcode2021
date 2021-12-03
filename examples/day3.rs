2b87571ecc25e00e17b5dc74be55e3536b451f05
Chris Emerson <github@mail.nosreme.org>
2021-12-03 06:28:03 +0000

    Incorrect part 2.

--- examples/day3.rs
+++ examples/day3.rs
@@ -1,4 +1,32 @@
 use adventofcode2021::{get_input,parse_lines};
+
+fn reduce_using<F:Fn(usize, usize) -> usize>(data: &[String], keep_digit: F) -> usize {
+    let mut col = 0;
+    let mut remaining = data.iter()
+        .map(|s| &s[..])
+        .collect::<Vec<&str>>();
+
+    while remaining.len() > 1 {
+        let mut zeros = Vec::new();
+        let mut ones = Vec::new();
+        for s in remaining {
+            if s.as_bytes()[col] == b'1' {
+                ones.push(s);
+            } else {
+                zeros.push(s);
+            }
+        }
+        remaining = match keep_digit(ones.len(), ones.len()+zeros.len()) {
+            0 => zeros,
+            1 => ones,
+            _ => unreachable!(),
+        };
+        col += 1;
+    }
+    assert_eq!(remaining.len(), 1);
+    return usize::from_str_radix(remaining[0], 2).unwrap();
+}
+
 fn main() -> std::io::Result<()>{
     let input = get_input(3)?;
 
@@ -28,20 +56,17 @@ fn main() -> std::io::Result<()>{
     println!("{}", gamma*epsilon);
 
     // Part 2
-    /*
-    println!("{}", increments.iter()
-                             .map(|mass| {
-                                 let mut mass = *mass;
-                                 let mut fuel = 0;
-                                 while mass > 5 {
-                                     let f = mass/3 - 2;
-                                     fuel += f;
-                                     mass = f;
-                                 }
-                                 fuel
-                              })
-                             .sum::<u32>());
-    */
+    let ox_gen_rating = reduce_using(
+        &data,
+        |ones, total| {
+            if ones*2 >= total { 1 } else { 0 }
+        });
+    let co2_scrubber_rating = reduce_using(
+        &data,
+        |ones, total| {
+            if ones*2 <= total { 0 } else { 1 }
+        });
+    println!("{}", ox_gen_rating*co2_scrubber_rating);
 
     Ok(())
 }
