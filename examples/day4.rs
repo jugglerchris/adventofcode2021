e2bd298477cb840a3c312c702bfe8baa747af2b4
Chris Emerson <github@mail.nosreme.org>
2021-12-04 06:37:05 +0000

    Day 4 done.

--- examples/day4.rs
+++ examples/day4.rs
@@ -4,15 +4,19 @@ type Number = usize;
 
 #[derive(Clone, Debug)]
 struct BingoCard {
-    data: [Number; 25],
+    data: [Option<Number>; 25],
     row_remain: [u8; 5],
     col_remain: [u8; 5],
 }
 
 impl BingoCard {
     pub fn from(ns: &[Number]) -> BingoCard {
+        let data = ns
+            .iter()
+            .map(|n| Some(*n))
+            .collect::<Vec<Option<Number>>>();
         BingoCard {
-            data: ns.try_into().unwrap(),
+            data: data.into_iter().collect::<Vec<_>>().try_into().unwrap(),
             row_remain: [5; 5],
             col_remain: [5; 5],
         }
@@ -22,13 +26,15 @@ impl BingoCard {
         match self.data
             .iter()
             .enumerate()
-            .find(|(_i, n)| **n == number)
+            .find(|(_i, n)| {
+                **n == Some(number)
+            })
         {
             None => false,
-            Some((i, n)) => {
+            Some((i, _n)) => {
                 let row = i/5;
                 let col = i%5;
-                self.data[i] = 0;
+                self.data[i] = None;
                 self.row_remain[row] -= 1;
                 self.col_remain[col] -= 1;
                 self.row_remain[row] == 0 || self.col_remain[col] == 0
@@ -37,16 +43,17 @@ impl BingoCard {
     }
 
     pub fn score(&self) -> usize {
-        self.data.iter().sum()
+        self.data
+            .iter()
+            .filter_map(|s| *s)
+            .sum()
     }
 }
 
-fn main() -> std::io::Result<()>{
-    let input = get_input(4)?;
-
+fn parse_input(input: &str) -> (Vec<Number>, Vec<BingoCard>) {
     let lines = input.lines().collect::<Vec<&str>>();
 
-    let numbers_called: Vec<usize> = lines[0].split(',')
+    let numbers_called: Vec<Number> = lines[0].split(',')
         .map(|s| s.parse().unwrap())
         .collect();
 
@@ -62,21 +69,53 @@ fn main() -> std::io::Result<()>{
         .map(BingoCard::from)
         .collect::<Vec<_>>();
 
-    // Part 1
-    {
-        let mut cards = cards.clone();
-        'outer: for n in &numbers_called {
-            for card in &mut cards {
+    (numbers_called, cards)
+}
+
+fn part1(numbers_called: &Vec<Number>, cards: &Vec<BingoCard>) -> usize {
+    let mut cards = cards.clone();
+    for n in numbers_called {
+        for card in &mut cards {
+            if card.call(*n) {
+                return card.score() * n;
+            }
+        }
+    }
+    unreachable!()
+}
+
+fn part2(numbers_called: &Vec<Number>, cards: &Vec<BingoCard>) -> usize {
+    let mut cards = cards.iter()
+        .map(|c| Some(c.clone()))
+        .collect::<Vec<_>>();
+    let mut cards_left = cards.len();
+    for n in numbers_called {
+        for maybe_card in &mut cards {
+            if let Some(card) = maybe_card {
                 if card.call(*n) {
-                    println!("Card called with score {}",
-                             card.score() * n);
-                    break 'outer;
+                    cards_left -= 1;
+                    if cards_left == 0 {
+                        return card.score() * n;
+                    } else {
+                        *maybe_card = None;
+                    }
                 }
             }
         }
     }
+    unreachable!()
+}
+
+fn main() -> std::io::Result<()>{
+    let input = get_input(4)?;
+
+    let (numbers_called, cards) = parse_input(&input);
+
+    // Part 1
+    println!("Part 1: {}", part1(&numbers_called, &cards));
 
     // Part 2
+    println!("Part 2: {}", part2(&numbers_called, &cards));
 
     Ok(())
 }
