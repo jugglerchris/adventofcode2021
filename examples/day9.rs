95247d8e9942bb551f14f9ed9737d217e9cb1bab
Chris Emerson <github@mail.nosreme.org>
2021-12-09 09:21:19 +0000

    Day 9 part 2.

--- examples/day9.rs
+++ examples/day9.rs
@@ -1,4 +1,5 @@
 use adventofcode2021::get_input;
+use std::collections::HashMap;
 
 fn part1(data: &Field) -> usize {
     let mut sum = 0;
@@ -8,7 +9,78 @@ fn part1(data: &Field) -> usize {
     sum
 }
 fn part2(data: &Field) -> usize {
-    unimplemented!()
+    let mut basin_map: HashMap<(usize, usize), usize> = HashMap::new();
+    let mut basins: Vec<Vec<(usize, usize)>> = Vec::new();
+    // Basin 0 is the 9s
+    basins.push(vec![]);
+
+    for y in 0..data.height {
+        for x in 0..data.width {
+            if basin_map.contains_key(&(x, y)) {
+                continue;
+            }
+            if data.get(x, y) == 9 {
+                basins[0].push((x, y));
+                basin_map.insert((x, y), 0);
+                continue;
+            }
+            // We're non-nine so check neighbours.
+            let (value_above, basin_above) = if y > 0 {
+                (data.get(x, y-1), *basin_map.get(&(x, y-1)).unwrap())
+            } else {
+                (9, 0)
+            };
+            let (value_left, basin_left) = if x > 0 {
+                (data.get(x-1, y), *basin_map.get(&(x-1, y)).unwrap())
+            } else {
+                (9, 0)
+            };
+            // To start, assume a new basin (but don't create it yet).
+            let mut basin = basins.len();
+            if value_above != 9 {
+                // Ok, join the basin above
+                basin = basin_above;
+            }
+            if value_left != 9 {
+                // Merge left basin into basin above
+                if basin == basin_above {
+                    let mut current_left_members = Vec::new();
+                    std::mem::swap(basins.get_mut(basin_left).unwrap(), &mut current_left_members);
+                    let new_basin = basins.get_mut(basin_above).unwrap();
+                    for (xx, yy) in current_left_members {
+                        new_basin.push((xx, yy));
+                        *basin_map.get_mut(&(xx, yy)).unwrap() = basin_above;
+                    }
+                    new_basin.push((x, y));
+                    basin_map.insert((x, y), basin_above);
+                } else {
+                    // Just add to left basin - above is 9.
+                    basins.get_mut(basin_left).unwrap().push((x, y));
+                    basin_map.insert((x, y), basin_left);
+                }
+            } else {
+                // Use the above value but no merging
+                if basin == basins.len() {
+                    basins.push(vec![(x, y)]);
+                } else {
+                    basins.get_mut(basin).unwrap().push((x, y));
+                }
+                basin_map.insert((x, y), basin);
+            }
+        }
+    }
+    #[cfg(test)]
+    for y in 0..data.height {
+        for x in 0..data.width {
+            print!("{}", *basin_map.get(&(x, y)).unwrap());
+        }
+        println!("");
+    }
+    let mut sizes = basins[1..].into_iter()
+                               .map(Vec::len)
+                               .collect::<Vec<usize>>();
+    sizes.sort_by(|a, b| b.cmp(a));
+    sizes[0] * sizes[1] * sizes[2]
 }
 
 struct Field {
@@ -79,7 +151,7 @@ fn test() {
     let data = Field::from(tests);
 
     assert_eq!(part1(&data), 15);
-//    assert_eq!(part2(&data), 0);
+    assert_eq!(part2(&data), 1134);
 }
 
 fn main() -> std::io::Result<()>{
