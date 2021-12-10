fb4f72bdfa1d4fafef2ccd19c795c29d5d3d8534
Chris Emerson <github@mail.nosreme.org>
2021-12-10 07:39:40 +0000

    Part 2 done.

--- examples/day10.rs
+++ examples/day10.rs
@@ -1,6 +1,14 @@
 use adventofcode2021::get_input;
 
-fn find_error(s: &str) -> Option<char>
+#[derive(Debug)]
+enum ParseResult {
+    Error(char),
+    Incomplete(Vec<char>),
+}
+
+use ParseResult::*;
+
+fn find_error(s: &str) -> ParseResult
 {
     let mut stack = Vec::new();
     for c in s.chars() {
@@ -12,31 +20,60 @@ fn find_error(s: &str) -> Option<char>
             ']' | ')' | '>' | '}' => {
                 let top = stack.pop().unwrap();
                 if top != c {
-                    return Some(c);
+                    return Error(c);
                 }
             }
             _ => panic!("Unknown character {}", c),
         }
     }
-    None
+    Incomplete(stack)
 }
 
 fn part1(data: &[&str]) -> usize {
     let mut score = 0;
     for line in data {
         match find_error(line) {
-            None => (),
-            Some(')') => { score += 3; }
-            Some(']') => { score += 57; }
-            Some('}') => { score += 1197; }
-            Some('>') => { score += 25137; }
+            Incomplete(_) => (),
+            Error(')') => { score += 3; }
+            Error(']') => { score += 57; }
+            Error('}') => { score += 1197; }
+            Error('>') => { score += 25137; }
             x => { panic!("{:?}", x); }
         }
     }
     return score;
 }
 fn part2(data: &[&str]) -> usize {
-    unimplemented!()
+    let mut scores = Vec::new();
+    for line in data {
+        match find_error(line) {
+            Error(_) => (),
+            Incomplete(mut v) => {
+                let mut score = 0usize;
+                while let Some(c) = v.pop() {
+                    match c {
+                        ')' => {
+                            score = score * 5 + 1;
+                        }
+                        ']' => {
+                            score = score * 5 + 2;
+                        }
+                        '}' => {
+                            score = score * 5 + 3;
+                        }
+                        '>' => {
+                            score = score * 5 + 4;
+                        }
+                        _ => panic!()
+                    }
+                }
+                scores.push(score);
+            }
+        }
+    }
+    scores.sort();
+    assert!(scores.len() & 1 == 1);
+    scores[scores.len() / 2]
 }
 
 #[test]
@@ -55,7 +92,7 @@ fn test() {
     let data: Vec<&str> = tests.lines().collect();
 
     assert_eq!(part1(&data), 26397);
-    assert_eq!(part2(&data), 0);
+    assert_eq!(part2(&data), 288957);
 }
 
 fn main() -> std::io::Result<()>{
