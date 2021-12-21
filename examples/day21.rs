cf5d66314333a3da5b72e0ceb2980645f01affd7
Chris Emerson <github@mail.nosreme.org>
2021-12-21 07:51:02 +0000

    Day 21 part 2 done.

--- examples/day21.rs
+++ examples/day21.rs
@@ -47,6 +47,7 @@ fn part1(data: &Data) -> usize {
     throws * score1
 }
 
+/*
 struct States {
     counts: Vec<usize>,
     player1_wins: usize,
@@ -114,11 +115,52 @@ impl States {
         dbg!(positions2);
     }
 }
+*/
+
+struct States1 {
+    counts: Vec<usize>,
+    wins: Vec<usize>,
+}
+impl States1 {
+    pub fn new() -> States1 {
+        let counts = vec![0; 10*21];
+        let wins = vec![];
+        States1 { counts, wins }
+    }
+    pub fn new_with_pos(pos: usize) -> States1 {
+        let mut result = States1::new();
+        result.add_state(pos, 0, 1);
+        result
+    }
+
+    pub fn add_state(&mut self, pos: usize, score: usize, count: usize) {
+        self.counts[(pos-1) + score*10] += count;
+    }
+    pub fn get_state_count(&self, pos: usize, score: usize) -> usize {
+        self.counts[(pos-1) + score*10]
+    }
+    pub fn add_wins(&mut self, turn: usize, count: usize) {
+        while self.wins.len() <= turn {
+            self.wins.push(0);
+        }
+        self.wins[turn] += count;
+    }
+    pub fn wins(&self) -> Vec<usize> {
+        self.wins.clone()
+    }
+    pub fn states_left(&self) -> usize {
+        self.counts.iter().cloned().sum()
+    }
+    pub fn max_states_left(&self) -> usize {
+        self.counts.iter().cloned().max().unwrap()
+    }
+    pub fn print_stats(&self) {
+        dbg!(&self.counts);
+        dbg!(&self.wins);
+    }
+}
 
 fn part2(data: &Data) -> usize {
-    let mut pos_counts = vec![0usize; 10*10];
-    pos_counts[(data.pos1-1) + 10*(data.pos2-1)] = 1;
-
     let mut moves = [0usize; 10];
     for a in 1..=3 {
         for b in 1..=3 {
@@ -127,54 +169,60 @@ fn part2(data: &Data) -> usize {
             }
         }
     }
-    dbg!(&moves);
 
-    let mut turn_player2 = false;
-    let mut state = States::new_with_pos(data.pos1, data.pos2);
-    while state.max_states_left() > 0 {
-        #[cfg(test)] dbg!(state.max_states_left());
-        #[cfg(test)] state.print_stats();
+    let mut state1 = States1::new_with_pos(data.pos1);
+    let mut state2 = States1::new_with_pos(data.pos2);
+    let mut turn = 0;
+    while state1.max_states_left() > 0 || state2.max_states_left() > 0 {
+        #[cfg(test)] dbg!(state1.max_states_left());
+        #[cfg(test)] dbg!(state2.max_states_left());
+        #[cfg(test)] state1.print_stats();
+        #[cfg(test)] state2.print_stats();
 
-        let mut newstate = States::new();
-        for pos1 in 1..=10 {
-            for pos2 in 1..=10 {
-                for score1 in 0..21 {
-                    for score2 in 0..21 {
-                        let count = state.get_state_count(pos1, pos2, score1, score2);
-                        for jump in 3..=9 {
-                            if !turn_player2 {
-                                let mut newpos = pos1 + jump;
-                                if newpos > 10 {
-                                    newpos -= 10;
-                                }
-                                let newscore = score1 + newpos;
-                                if newscore >= 21 {
-                                    newstate.add_wins(count * moves[jump], turn_player2);
-                                } else {
-                                    newstate.add_state(newpos, pos2, newscore, score2, count * moves[jump] );
-                                }
-                            } else {
-                                let mut newpos = pos2 + jump;
-                                if newpos > 10 {
-                                    newpos -= 10;
-                                }
-                                let newscore = score2 + newpos;
-                                if newscore >= 21 {
-                                    newstate.add_wins(count * moves[jump], turn_player2);
-                                } else {
-                                    newstate.add_state(pos1, newpos, score2, score2, count * moves[jump] );
-                                }
-                            }
-                        }
+        let mut newstate1 = States1::new();
+        std::mem::swap(&mut newstate1.wins, &mut state1.wins);
+        let mut newstate2 = States1::new();
+        std::mem::swap(&mut newstate2.wins, &mut state2.wins);
+        for pos in 1..=10 {
+            for score in 0..21 {
+                let count1 = state1.get_state_count(pos, score);
+                let count2 = state2.get_state_count(pos, score);
+                for jump in 3..=9 {
+                    let mut newpos = pos + jump;
+                    if newpos > 10 {
+                        newpos -= 10;
+                    }
+                    let newscore = score + newpos;
+                    if newscore >= 21 {
+                        newstate1.add_wins(turn, count1 * moves[jump]);
+                        newstate2.add_wins(turn, count2 * moves[jump]);
+                    } else {
+                        newstate1.add_state(newpos, newscore, count1 * moves[jump]);
+                        newstate2.add_state(newpos, newscore, count2 * moves[jump]);
                     }
                 }
             }
         }
-        state = newstate;
-        turn_player2 = !turn_player2;
+
+        turn += 1;
+        state1 = newstate1;
+        state2 = newstate2;
     }
-    let (wins1, wins2) = state.wins();
-    wins1.max(wins2)
+    let mut tot_wins1 = 0;
+    let mut tot_wins2 = 0;
+    let wins1 = state1.wins();
+    let wins2 = state2.wins();
+
+    let mut states_p1 = 1;
+    let mut states_p2 = 1;
+    for (t, (w1, w2)) in wins1.into_iter().zip(wins2.into_iter()).enumerate() {
+        tot_wins1 += w1 * states_p2;
+        states_p1 = states_p1 * 27 - w1;
+
+        tot_wins2 += w2 * states_p1;
+        states_p2 = states_p2 * 27 - w2;
+    }
+    tot_wins1.max(tot_wins2)
 }
 
 #[test]
@@ -185,7 +233,7 @@ Player 2 starting position: 8
     let data = parse_input(&tests);
 
     assert_eq!(part1(&data), 739785);
-    assert_eq!(part2(&data), 0);
+    assert_eq!(part2(&data), 444356092776315);
 }
 
 fn main() -> std::io::Result<()>{
