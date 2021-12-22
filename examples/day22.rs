76e0f3ed9d68e12dec26c89cd8144d82a829707d
Chris Emerson <github@mail.nosreme.org>
2021-12-22 09:54:49 +0000

    Day 22 part 2.

--- examples/day22.rs
+++ examples/day22.rs
@@ -1,7 +1,7 @@
 use adventofcode2021::{get_input,parse_lines,regex_parser};
 use std::collections::HashMap;
 
-#[derive(Debug,Clone)]
+#[derive(Debug,Clone,PartialEq,Eq)]
 pub struct Step {
     on: bool,
     x0: isize,
@@ -52,8 +52,86 @@ fn part1(data: &Data) -> usize {
     }
     cubes.into_values().filter(|b| *b).count()
 }
-fn part2(data: &Data) -> usize {
-    unimplemented!()
+
+type Cube = Step;
+
+impl Step {
+    fn range_overlaps(a0: isize, a1: isize, b0: isize, b1: isize) -> bool {
+        a0 <= b1 && a1 >= b0
+    }
+    pub fn overlaps(&self, other: &Step) -> bool {
+        Step::range_overlaps(self.x0, self.x1, other.x0, other.x1)
+            && Step::range_overlaps(self.y0, self.y1, other.y0, other.y1)
+            && Step::range_overlaps(self.z0, self.z1, other.z0, other.z1)
+    }
+
+    fn split_range(mut our_a0: isize, mut our_a1: isize, their_a0: isize, their_a1: isize) -> Vec<(isize, isize)>
+    {
+        let mut result = Vec::new();
+        if our_a0 < their_a0 {
+            // Add the non-overlapping part at the start
+            result.push((our_a0, their_a0-1));
+            our_a0 = their_a0;
+        }
+        if our_a1 > their_a1 {
+            // Add the non-overlapping piece at the end
+            result.push((their_a1+1, our_a1));
+            our_a1 = their_a1;
+        }
+        // Center overlapping piece
+        result.push((our_a0, our_a1));
+        result
+    }
+    pub fn split(&self, other: &Step) -> Vec<Step> {
+        let mut results = Vec::new();
+        let xranges = Step::split_range(self.x0, self.x1, other.x0, other.x1);
+        let yranges = Step::split_range(self.y0, self.y1, other.y0, other.y1);
+        let zranges = Step::split_range(self.z0, self.z1, other.z0, other.z1);
+        let on = self.on;
+        for (x0, x1) in &xranges {
+            for (y0, y1) in &yranges {
+                for (z0, z1) in &zranges {
+                    results.push(Cube { on, x0: *x0, x1: *x1, y0: *y0, y1: *y1, z0: *z0, z1: *z1 });
+                }
+            }
+        }
+        results
+    }
+    pub fn volume(&self) -> isize {
+        (self.x1 - self.x0 + 1) *
+          (self.y1 - self.y0 + 1) *
+          (self.z1 - self.z0 + 1)
+    }
+}
+
+fn part2(data: &Data) -> isize {
+    // Vector of non-overlapping on cubes
+    let mut on_cubes: Vec<Cube> = vec![];
+    for newcube in data {
+        let mut new_cubes = Vec::new();
+        for cube in on_cubes {
+            if newcube.overlaps(&cube) {
+                // Split into pieces which aren't overlapped and push those;
+                // Pieces which are overlapped will replaced by the new cube.
+                let pieces = cube.split(&newcube);
+                for piece in pieces {
+                    if !piece.overlaps(newcube) {
+                        new_cubes.push(piece);
+                    }
+                }
+            } else {
+                // This one's unaffected.
+                new_cubes.push(cube);
+            }
+        }
+        if newcube.on {
+            new_cubes.push(newcube.clone());
+        }
+        on_cubes = new_cubes;
+    }
+    on_cubes.into_iter()
+            .map(|cube| cube.volume())
+            .sum()
 }
 
 #[test]
@@ -83,7 +161,69 @@ on x=967..23432,y=45373..81175,z=27513..53682"#;
     let data = parse_input(&tests);
 
     assert_eq!(part1(&data), 590784);
-    assert_eq!(part2(&data), 0);
+
+    let test2 = r#"on x=-5..47,y=-31..22,z=-19..33
+on x=-44..5,y=-27..21,z=-14..35
+on x=-49..-1,y=-11..42,z=-10..38
+on x=-20..34,y=-40..6,z=-44..1
+off x=26..39,y=40..50,z=-2..11
+on x=-41..5,y=-41..6,z=-36..8
+off x=-43..-33,y=-45..-28,z=7..25
+on x=-33..15,y=-32..19,z=-34..11
+off x=35..47,y=-46..-34,z=-11..5
+on x=-14..36,y=-6..44,z=-16..29
+on x=-57795..-6158,y=29564..72030,z=20435..90618
+on x=36731..105352,y=-21140..28532,z=16094..90401
+on x=30999..107136,y=-53464..15513,z=8553..71215
+on x=13528..83982,y=-99403..-27377,z=-24141..23996
+on x=-72682..-12347,y=18159..111354,z=7391..80950
+on x=-1060..80757,y=-65301..-20884,z=-103788..-16709
+on x=-83015..-9461,y=-72160..-8347,z=-81239..-26856
+on x=-52752..22273,y=-49450..9096,z=54442..119054
+on x=-29982..40483,y=-108474..-28371,z=-24328..38471
+on x=-4958..62750,y=40422..118853,z=-7672..65583
+on x=55694..108686,y=-43367..46958,z=-26781..48729
+on x=-98497..-18186,y=-63569..3412,z=1232..88485
+on x=-726..56291,y=-62629..13224,z=18033..85226
+on x=-110886..-34664,y=-81338..-8658,z=8914..63723
+on x=-55829..24974,y=-16897..54165,z=-121762..-28058
+on x=-65152..-11147,y=22489..91432,z=-58782..1780
+on x=-120100..-32970,y=-46592..27473,z=-11695..61039
+on x=-18631..37533,y=-124565..-50804,z=-35667..28308
+on x=-57817..18248,y=49321..117703,z=5745..55881
+on x=14781..98692,y=-1341..70827,z=15753..70151
+on x=-34419..55919,y=-19626..40991,z=39015..114138
+on x=-60785..11593,y=-56135..2999,z=-95368..-26915
+on x=-32178..58085,y=17647..101866,z=-91405..-8878
+on x=-53655..12091,y=50097..105568,z=-75335..-4862
+on x=-111166..-40997,y=-71714..2688,z=5609..50954
+on x=-16602..70118,y=-98693..-44401,z=5197..76897
+on x=16383..101554,y=4615..83635,z=-44907..18747
+off x=-95822..-15171,y=-19987..48940,z=10804..104439
+on x=-89813..-14614,y=16069..88491,z=-3297..45228
+on x=41075..99376,y=-20427..49978,z=-52012..13762
+on x=-21330..50085,y=-17944..62733,z=-112280..-30197
+on x=-16478..35915,y=36008..118594,z=-7885..47086
+off x=-98156..-27851,y=-49952..43171,z=-99005..-8456
+off x=2032..69770,y=-71013..4824,z=7471..94418
+on x=43670..120875,y=-42068..12382,z=-24787..38892
+off x=37514..111226,y=-45862..25743,z=-16714..54663
+off x=25699..97951,y=-30668..59918,z=-15349..69697
+off x=-44271..17935,y=-9516..60759,z=49131..112598
+on x=-61695..-5813,y=40978..94975,z=8655..80240
+off x=-101086..-9439,y=-7088..67543,z=33935..83858
+off x=18020..114017,y=-48931..32606,z=21474..89843
+off x=-77139..10506,y=-89994..-18797,z=-80..59318
+off x=8476..79288,y=-75520..11602,z=-96624..-24783
+on x=-47488..-1262,y=24338..100707,z=16292..72967
+off x=-84341..13987,y=2429..92914,z=-90671..-1318
+off x=-37810..49457,y=-71013..-7894,z=-105357..-13188
+off x=-27365..46395,y=31009..98017,z=15428..76570
+off x=-70369..-16548,y=22648..78696,z=-1892..86821
+on x=-53470..21291,y=-120233..-33476,z=-44150..38147
+off x=-93533..-4276,y=-16170..68771,z=-104985..-24507"#;
+    let data2 = parse_input(&test2);
+    assert_eq!(part2(&data2), 2758514936282235);
 }
 
 fn main() -> std::io::Result<()>{
